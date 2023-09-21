4f42f3680e6d7ea43809fa303d02d685fd6caa5f
Pavel <jrmarty@mail.ru>
2023-09-21 18:53:32 +0300

    added FSD and absolute paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,13 @@
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
-
-
 
     return (
         <div className={classNames('app', {}, [theme])}>
@@ -18,8 +16,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
            <Suspense fallback={<div>Loading...</div>}>
                <Routes>
-                   <Route path={'/about'} element={<AboutPageAsync/>}/>
-                   <Route path={'/'} element={<MainPageAsync/>}/>
+                   <Route path={'/about'} element={<AboutPage/>}/>
+                   <Route path={'/'} element={<MainPage/>}/>
                </Routes>
            </Suspense>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import {render} from "react-dom";
 import React from "react";
-import {Counter} from "./components/Counter";
-import App from "./App";
+import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "./theme/ThemeProvider";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
 
 render(
