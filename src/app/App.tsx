a4b8ff18ff611c69e5299152993067fb8f2566a0
Pavel <jrmarty@mail.ru>
2023-09-22 20:42:34 +0300

    added route config, navbar and themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,22 +5,17 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-           <Suspense fallback={<div>Loading...</div>}>
-               <Routes>
-                   <Route path={'/about'} element={<AboutPage/>}/>
-                   <Route path={'/'} element={<MainPage/>}/>
-               </Routes>
-           </Suspense>
-
         </div>
     );
 };
