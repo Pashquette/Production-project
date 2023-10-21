283b22674a88934a3ee7a26e99698fd80fd8763b
Pavel <pashquette@gmail.com>
2023-10-21 18:53:49 +0300

    add loki test ui

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,6 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import 'loki/configure-react';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
